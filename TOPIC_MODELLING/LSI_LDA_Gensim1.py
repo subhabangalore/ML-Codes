29a68d46a546a8b963c6264a67fedd984c3e57c1
subhabangalore <subhabangalore@gmail.com>
2019-06-01 17:22:09 +0530

    Topic Modelling Files
    Topic Modelling Files with LDA and LSI

---
+++ TOPIC_MODELLING/LSI_LDA_Gensim1.py
@@ -0,0 +1,125 @@
+﻿Welcome to Canopy's interactive data-analysis environment!
+
+Kernel running in the 'User' environment.
+
+Pylab is active using Qt4Agg.
+
+Python 2.7.13 |Enthought, Inc. (x86_64)| (default, Mar  2 2017, 16:05:12) [MSC v.1500 64 bit (AMD64)]
+Type "copyright", "credits" or "license" for more information.
+
+IPython 5.3.0 -- An enhanced Interactive Python.
+?         -> Introduction and overview of IPython's features.
+%quickref -> Quick reference.
+help      -> Python's own help system.
+object?   -> Details about 'object', use 'object??' for extra details.
+
+import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+from gensim import corpora
+
+documents = ["Human machine interface for lab abc computer applications",
+              "A survey of user opinion of computer system response time",
+              "The EPS user interface management system",
+              "System and human system engineering testing of EPS",
+              "Relation of user perceived response time to error measurement",
+              "The generation of random binary unordered trees",
+              "The intersection graph of paths in trees",
+              "Graph minors IV Widths of trees and well quasi ordering",
+              "Graph minors A survey"]
+
+stoplist = set('for a of the and to in'.split())
+
+texts = [[word for word in document.lower().split() if word not in stoplist]
+       for document in documents]
+
+from collections import defaultdict
+
+frequency = defaultdict(int)
+
+for text in texts:
+     for token in text:
+         frequency[token] += 1
+        
+
+texts = [[token for token in text if frequency[token] > 1]
+          for text in texts]
+
+dictionary = corpora.Dictionary(texts)
+2019-01-06 13:49:16,407 : INFO : adding document #0 to Dictionary(0 unique tokens: [])
+2019-01-06 13:49:16,423 : INFO : built Dictionary(12 unique tokens: [u'minors', u'graph', u'system', u'trees', u'eps']...) from 9 documents (total 29 corpus positions)
+
+dictionary.save('deerwester.dict')
+2019-01-06 13:50:05,493 : INFO : saving Dictionary object under deerwester.dict, separately None
+2019-01-06 13:50:05,572 : INFO : saved deerwester.dict
+
+new_doc = "Human computer interaction"
+
+new_vec = dictionary.doc2bow(new_doc.lower().split())
+
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+from gensim import corpora, models, similarities
+if (os.path.exists("deerwester.dict")):
+    dictionary = corpora.Dictionary.load('deerwester.dict')
+    corpus = corpora.MmCorpus('deerwester.mm')
+    print("Used files generated from first tutorial")
+else:
+  print("Please run first tutorial to generate data set")
+  
+2019-01-06 14:00:04,647 : INFO : loading Dictionary object from deerwester.dict
+2019-01-06 14:00:04,661 : INFO : loaded deerwester.dict
+2019-01-06 14:00:04,661 : INFO : loaded corpus index from deerwester.mm.index
+2019-01-06 14:00:04,770 : INFO : initializing cython corpus reader from deerwester.mm
+2019-01-06 14:00:04,786 : INFO : accepted corpus with 9 documents, 12 features, 28 non-zero entries
+Used files generated from first tutorial
+
+tfidf = models.TfidfModel(corpus)
+2019-01-06 14:00:32,446 : INFO : collecting document frequencies
+2019-01-06 14:00:32,571 : INFO : PROGRESS: processing document #0
+2019-01-06 14:00:32,585 : INFO : calculating IDF weights for 9 documents and 11 features (28 matrix non-zeros)
+
+doc_bow = [(0, 1), (1, 1)]
+
+corpus_tfidf = tfidf[corpus]
+
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
+2019-01-06 14:01:49,026 : INFO : using serial LSI version on this node
+2019-01-06 14:01:49,026 : INFO : updating model with new documents
+2019-01-06 14:01:49,026 : INFO : preparing a new chunk of documents
+2019-01-06 14:01:49,088 : INFO : using 100 extra samples and 2 power iterations
+2019-01-06 14:01:49,104 : INFO : 1st phase: constructing (12L, 102L) action matrix
+2019-01-06 14:01:49,197 : INFO : orthonormalizing (12L, 102L) action matrix
+2019-01-06 14:01:49,588 : INFO : 2nd phase: running dense svd on (12L, 9L) matrix
+2019-01-06 14:01:50,025 : INFO : computing the final decomposition
+2019-01-06 14:01:50,072 : INFO : keeping 2 factors (discarding 47.565% of energy spectrum)
+2019-01-06 14:01:50,086 : INFO : processed documents up to #9
+2019-01-06 14:01:50,118 : INFO : topic #0(1.594): 0.703*"trees" + 0.538*"graph" + 0.402*"minors" + 0.187*"survey" + 0.061*"system" + 0.060*"time" + 0.060*"response" + 0.058*"user" + 0.049*"computer" + 0.035*"interface"
+2019-01-06 14:01:50,134 : INFO : topic #1(1.476): -0.460*"system" + -0.373*"user" + -0.332*"eps" + -0.328*"interface" + -0.320*"response" + -0.320*"time" + -0.293*"computer" + -0.280*"human" + -0.171*"survey" + 0.161*"trees"
+
+corpus_lsi = lsi[corpus_tfidf]
+
+lsi.print_topics(2)
+2019-01-06 14:02:36,247 : INFO : topic #0(1.594): 0.703*"trees" + 0.538*"graph" + 0.402*"minors" + 0.187*"survey" + 0.061*"system" + 0.060*"time" + 0.060*"response" + 0.058*"user" + 0.049*"computer" + 0.035*"interface"
+2019-01-06 14:02:36,263 : INFO : topic #1(1.476): -0.460*"system" + -0.373*"user" + -0.332*"eps" + -0.328*"interface" + -0.320*"response" + -0.320*"time" + -0.293*"computer" + -0.280*"human" + -0.171*"survey" + 0.161*"trees"
+Out[38]: 
+[(0,
+  u'0.703*"trees" + 0.538*"graph" + 0.402*"minors" + 0.187*"survey" + 0.061*"system" + 0.060*"time" + 0.060*"response" + 0.058*"user" + 0.049*"computer" + 0.035*"interface"'),
+ (1,
+  u'-0.460*"system" + -0.373*"user" + -0.332*"eps" + -0.328*"interface" + -0.320*"response" + -0.320*"time" + -0.293*"computer" + -0.280*"human" + -0.171*"survey" + 0.161*"trees"')]
+
+lda=models.LdaModel(corpus, id2word=dictionary, num_topics=10)
+2019-01-06 14:04:32,342 : INFO : using symmetric alpha at 0.1
+2019-01-06 14:04:32,358 : INFO : using symmetric eta at 0.1
+2019-01-06 14:04:32,358 : INFO : using serial LDA version on this node
+2019-01-06 14:04:32,515 : INFO : running online (single-pass) LDA training, 10 topics, 1 passes over the supplied corpus of 9 documents, updating model once every 9 documents, evaluating perplexity every 9 documents, iterating 50x with a convergence threshold of 0.001000
+2019-01-06 14:04:32,529 : WARNING : too few updates, training might not converge; consider increasing the number of passes or iterations to improve accuracy
+2019-01-06 14:04:32,624 : INFO : -8.215 per-word bound, 297.2 perplexity estimate based on a held-out corpus of 9 documents with 29 words
+2019-01-06 14:04:32,624 : INFO : PROGRESS: pass 0, at document #9/9
+2019-01-06 14:04:32,654 : INFO : topic #6 (0.100): 0.206*"computer" + 0.108*"user" + 0.108*"system" + 0.108*"survey" + 0.108*"time" + 0.108*"response" + 0.108*"human" + 0.108*"interface" + 0.010*"trees" + 0.010*"graph"
+2019-01-06 14:04:32,654 : INFO : topic #9 (0.100): 0.083*"trees" + 0.083*"minors" + 0.083*"graph" + 0.083*"system" + 0.083*"user" + 0.083*"eps" + 0.083*"interface" + 0.083*"human" + 0.083*"response" + 0.083*"survey"
+2019-01-06 14:04:32,654 : INFO : topic #3 (0.100): 0.206*"system" + 0.206*"trees" + 0.206*"graph" + 0.108*"eps" + 0.108*"human" + 0.108*"minors" + 0.010*"user" + 0.010*"interface" + 0.010*"time" + 0.010*"response"
+2019-01-06 14:04:32,654 : INFO : topic #5 (0.100): 0.083*"trees" + 0.083*"graph" + 0.083*"user" + 0.083*"system" + 0.083*"minors" + 0.083*"human" + 0.083*"interface" + 0.083*"response" + 0.083*"eps" + 0.083*"survey"
+2019-01-06 14:04:32,671 : INFO : topic #2 (0.100): 0.083*"trees" + 0.083*"system" + 0.083*"graph" + 0.083*"user" + 0.083*"human" + 0.083*"minors" + 0.083*"response" + 0.083*"time" + 0.083*"interface" + 0.083*"survey"
+2019-01-06 14:04:32,671 : INFO : topic diff=6.339295, rho=1.000000
---
+++ TOPIC_MODELLING/Result_LDA1.py
@@ -0,0 +1,193 @@
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:22:17) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:\Python27\Sept_Oct17\LDAlda1.py =================
+('church', 'pope', 'years', 'people', 'mother', 'last', 'told', 'first', 'world', 'year')
+('0 UK: Prince Charles spearheads British royal revolution. LONDON 1996-08-20', '1 GERMANY: Historic Dresden church rising from WW2 ashes. DRESDEN, Germany 1996-08-21', "2 INDIA: Mother Teresa's condition said still unstable. CALCUTTA 1996-08-23", '3 UK: Palace warns British weekly over Charles pictures. LONDON 1996-08-25', '4 INDIA: Mother Teresa, slightly stronger, blesses nuns. CALCUTTA 1996-08-25', "5 INDIA: Mother Teresa's condition unchanged, thousands pray. CALCUTTA 1996-08-25", '6 INDIA: Mother Teresa shows signs of strength, blesses nuns. CALCUTTA 1996-08-26', "7 INDIA: Mother Teresa's condition improves, many pray. CALCUTTA, India 1996-08-25", '8 INDIA: Mother Teresa improves, nuns pray for "miracle". CALCUTTA 1996-08-26', '9 UK: Charles under fire over prospect of Queen Camilla. LONDON 1996-08-26')
+INFO:lda:n_documents: 395
+INFO:lda:vocab_size: 4258
+INFO:lda:n_words: 84010
+INFO:lda:n_topics: 20
+INFO:lda:n_iter: 1500
+INFO:lda:<0> log likelihood: -1051748
+INFO:lda:<10> log likelihood: -719800
+INFO:lda:<20> log likelihood: -699115
+INFO:lda:<30> log likelihood: -689370
+INFO:lda:<40> log likelihood: -684918
+INFO:lda:<50> log likelihood: -681322
+INFO:lda:<60> log likelihood: -678979
+INFO:lda:<70> log likelihood: -676598
+INFO:lda:<80> log likelihood: -675383
+INFO:lda:<90> log likelihood: -673316
+INFO:lda:<100> log likelihood: -672761
+INFO:lda:<110> log likelihood: -671320
+INFO:lda:<120> log likelihood: -669744
+INFO:lda:<130> log likelihood: -669292
+INFO:lda:<140> log likelihood: -667940
+INFO:lda:<150> log likelihood: -668038
+INFO:lda:<160> log likelihood: -667429
+INFO:lda:<170> log likelihood: -666475
+INFO:lda:<180> log likelihood: -665562
+INFO:lda:<190> log likelihood: -664920
+INFO:lda:<200> log likelihood: -664979
+INFO:lda:<210> log likelihood: -664722
+INFO:lda:<220> log likelihood: -664459
+INFO:lda:<230> log likelihood: -664360
+INFO:lda:<240> log likelihood: -663600
+INFO:lda:<250> log likelihood: -664164
+INFO:lda:<260> log likelihood: -663826
+INFO:lda:<270> log likelihood: -663458
+INFO:lda:<280> log likelihood: -663393
+INFO:lda:<290> log likelihood: -662904
+INFO:lda:<300> log likelihood: -662294
+INFO:lda:<310> log likelihood: -662031
+INFO:lda:<320> log likelihood: -662430
+INFO:lda:<330> log likelihood: -661601
+INFO:lda:<340> log likelihood: -662108
+INFO:lda:<350> log likelihood: -662152
+INFO:lda:<360> log likelihood: -661899
+INFO:lda:<370> log likelihood: -661012
+INFO:lda:<380> log likelihood: -661278
+INFO:lda:<390> log likelihood: -661085
+INFO:lda:<400> log likelihood: -660418
+INFO:lda:<410> log likelihood: -660510
+INFO:lda:<420> log likelihood: -660343
+INFO:lda:<430> log likelihood: -659789
+INFO:lda:<440> log likelihood: -659336
+INFO:lda:<450> log likelihood: -659039
+INFO:lda:<460> log likelihood: -659329
+INFO:lda:<470> log likelihood: -658707
+INFO:lda:<480> log likelihood: -658879
+INFO:lda:<490> log likelihood: -658819
+INFO:lda:<500> log likelihood: -658407
+INFO:lda:<510> log likelihood: -658651
+INFO:lda:<520> log likelihood: -658111
+INFO:lda:<530> log likelihood: -658018
+INFO:lda:<540> log likelihood: -658111
+INFO:lda:<550> log likelihood: -657925
+INFO:lda:<560> log likelihood: -657860
+INFO:lda:<570> log likelihood: -657494
+INFO:lda:<580> log likelihood: -657723
+INFO:lda:<590> log likelihood: -657591
+INFO:lda:<600> log likelihood: -657557
+INFO:lda:<610> log likelihood: -657505
+INFO:lda:<620> log likelihood: -657730
+INFO:lda:<630> log likelihood: -657304
+INFO:lda:<640> log likelihood: -657208
+INFO:lda:<650> log likelihood: -657518
+INFO:lda:<660> log likelihood: -657541
+INFO:lda:<670> log likelihood: -657381
+INFO:lda:<680> log likelihood: -657575
+INFO:lda:<690> log likelihood: -656985
+INFO:lda:<700> log likelihood: -656815
+INFO:lda:<710> log likelihood: -656930
+INFO:lda:<720> log likelihood: -656538
+INFO:lda:<730> log likelihood: -656291
+INFO:lda:<740> log likelihood: -656417
+INFO:lda:<750> log likelihood: -656747
+INFO:lda:<760> log likelihood: -656600
+INFO:lda:<770> log likelihood: -656269
+INFO:lda:<780> log likelihood: -656311
+INFO:lda:<790> log likelihood: -656069
+INFO:lda:<800> log likelihood: -656228
+INFO:lda:<810> log likelihood: -656178
+INFO:lda:<820> log likelihood: -655694
+INFO:lda:<830> log likelihood: -655997
+INFO:lda:<840> log likelihood: -656224
+INFO:lda:<850> log likelihood: -656197
+INFO:lda:<860> log likelihood: -655889
+INFO:lda:<870> log likelihood: -656180
+INFO:lda:<880> log likelihood: -656997
+INFO:lda:<890> log likelihood: -655989
+INFO:lda:<900> log likelihood: -655615
+INFO:lda:<910> log likelihood: -655584
+INFO:lda:<920> log likelihood: -656602
+INFO:lda:<930> log likelihood: -656083
+INFO:lda:<940> log likelihood: -656294
+INFO:lda:<950> log likelihood: -656257
+INFO:lda:<960> log likelihood: -656243
+INFO:lda:<970> log likelihood: -656028
+INFO:lda:<980> log likelihood: -655603
+INFO:lda:<990> log likelihood: -656012
+INFO:lda:<1000> log likelihood: -655849
+INFO:lda:<1010> log likelihood: -655376
+INFO:lda:<1020> log likelihood: -655417
+INFO:lda:<1030> log likelihood: -655856
+INFO:lda:<1040> log likelihood: -655197
+INFO:lda:<1050> log likelihood: -655938
+INFO:lda:<1060> log likelihood: -655529
+INFO:lda:<1070> log likelihood: -655092
+INFO:lda:<1080> log likelihood: -655119
+INFO:lda:<1090> log likelihood: -656215
+INFO:lda:<1100> log likelihood: -655602
+INFO:lda:<1110> log likelihood: -655296
+INFO:lda:<1120> log likelihood: -655547
+INFO:lda:<1130> log likelihood: -655580
+INFO:lda:<1140> log likelihood: -655604
+INFO:lda:<1150> log likelihood: -655168
+INFO:lda:<1160> log likelihood: -655281
+INFO:lda:<1170> log likelihood: -655409
+INFO:lda:<1180> log likelihood: -655517
+INFO:lda:<1190> log likelihood: -654922
+INFO:lda:<1200> log likelihood: -655304
+INFO:lda:<1210> log likelihood: -655852
+INFO:lda:<1220> log likelihood: -655184
+INFO:lda:<1230> log likelihood: -655650
+INFO:lda:<1240> log likelihood: -655606
+INFO:lda:<1250> log likelihood: -656086
+INFO:lda:<1260> log likelihood: -655698
+INFO:lda:<1270> log likelihood: -655351
+INFO:lda:<1280> log likelihood: -655686
+INFO:lda:<1290> log likelihood: -654801
+INFO:lda:<1300> log likelihood: -654973
+INFO:lda:<1310> log likelihood: -655186
+INFO:lda:<1320> log likelihood: -655128
+INFO:lda:<1330> log likelihood: -655365
+INFO:lda:<1340> log likelihood: -655338
+INFO:lda:<1350> log likelihood: -655219
+INFO:lda:<1360> log likelihood: -655115
+INFO:lda:<1370> log likelihood: -654930
+INFO:lda:<1380> log likelihood: -655209
+INFO:lda:<1390> log likelihood: -654940
+INFO:lda:<1400> log likelihood: -655055
+INFO:lda:<1410> log likelihood: -655286
+INFO:lda:<1420> log likelihood: -655316
+INFO:lda:<1430> log likelihood: -655257
+INFO:lda:<1440> log likelihood: -654964
+INFO:lda:<1450> log likelihood: -654884
+INFO:lda:<1460> log likelihood: -655493
+INFO:lda:<1470> log likelihood: -655415
+INFO:lda:<1480> log likelihood: -655192
+INFO:lda:<1490> log likelihood: -655728
+INFO:lda:<1499> log likelihood: -655858
+Topic 0: british churchill sale million major letters west britain
+Topic 1: church government political country state people party against
+Topic 2: elvis king fans presley life concert young death
+Topic 3: yeltsin russian russia president kremlin moscow michael operation
+Topic 4: pope vatican paul john surgery hospital pontiff rome
+Topic 5: family funeral police miami versace cunanan city service
+Topic 6: simpson former years court president wife south church
+Topic 7: order mother successor election nuns church nirmala head
+Topic 8: charles prince diana royal king queen parker bowles
+Topic 9: film french france against bardot paris poster animal
+Topic 10: germany german war nazi letter christian book jews
+Topic 11: east peace prize award timor quebec belo leader
+Topic 12: n't life show told very love television father
+Topic 13: years year time last church world people say
+Topic 14: mother teresa heart calcutta charity nun hospital missionaries
+Topic 15: city salonika capital buddhist cultural vietnam byzantine show
+Topic 16: music tour opera singer israel people film israeli
+Topic 17: church catholic bernardin cardinal bishop wright death cancer
+Topic 18: harriman clinton u.s ambassador paris president churchill france
+Topic 19: city museum art exhibition century million churches set
+0 UK: Prince Charles spearheads British royal revolution. LONDON 1996-08-20 (top topic: 8)
+1 GERMANY: Historic Dresden church rising from WW2 ashes. DRESDEN, Germany 1996-08-21 (top topic: 13)
+2 INDIA: Mother Teresa's condition said still unstable. CALCUTTA 1996-08-23 (top topic: 14)
+3 UK: Palace warns British weekly over Charles pictures. LONDON 1996-08-25 (top topic: 8)
+4 INDIA: Mother Teresa, slightly stronger, blesses nuns. CALCUTTA 1996-08-25 (top topic: 14)
+5 INDIA: Mother Teresa's condition unchanged, thousands pray. CALCUTTA 1996-08-25 (top topic: 14)
+6 INDIA: Mother Teresa shows signs of strength, blesses nuns. CALCUTTA 1996-08-26 (top topic: 14)
+7 INDIA: Mother Teresa's condition improves, many pray. CALCUTTA, India 1996-08-25 (top topic: 14)
+8 INDIA: Mother Teresa improves, nuns pray for "miracle". CALCUTTA 1996-08-26 (top topic: 14)
+9 UK: Charles under fire over prospect of Queen Camilla. LONDON 1996-08-26 (top topic: 8)
+>>> 
